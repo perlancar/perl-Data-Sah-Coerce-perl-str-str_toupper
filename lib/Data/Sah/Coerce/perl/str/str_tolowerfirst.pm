fa52a97aa7844cd65f0a1527f363c76170a56748
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-03-08 09:33:26 +0700

    Initial commit

---
+++ lib/Data/Sah/Coerce/perl/str/str_tolowerfirst.pm
@@ -0,0 +1,43 @@
+package Data::Sah::Coerce::perl::str::str_tolowerfirst;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub meta {
+    +{
+        v => 2,
+        enable_by_default => 0,
+        prio => 50,
+    };
+}
+
+sub coerce {
+    my %args = @_;
+
+    my $dt = $args{data_term};
+
+    my $res = {};
+
+    $res->{expr_match} = "!ref($dt)";
+    $res->{expr_coerce} = join(
+        "",
+        "lcfirst($dt)",
+    );
+
+    $res;
+}
+
+1;
+# ABSTRACT: Coerce string's first character to lower case
+
+=for Pod::Coverage ^(meta|coerce)$
+
+=head1 DESCRIPTION
+
+The rule is not enabled by default. You can enable it in a schema using e.g.:
+
+ ["str", "x.perl.coerce_rules"=>["str_tolowerfirst"]]
